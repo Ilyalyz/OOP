a58883de4535a78fc1f40a263a10552ce0a2107b
Илья <poltavecilya@gmail.com>
2025-06-27 06:21:43 +0300

    Add files via upload

---
+++ lab18/Program.cs
@@ -0,0 +1,113 @@
+﻿// Завдання 1
+/*
+using System;
+using System.Collections.Generic;
+
+interface IAnimal
+{
+    void Speak();
+    void Eat();
+}
+
+class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Собака каже: Гав!");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Собака їсть кістку");
+    }
+}
+
+class Cat : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Кішка каже: Няв!");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Кішка їсть корм");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<IAnimal> animals = new List<IAnimal>
+        {
+            new Dog(),
+            new Cat()
+        };
+
+        foreach (var animal in animals)
+        {
+            animal.Speak();
+            animal.Eat();
+            Console.WriteLine();
+        }
+    }
+}
+*/
+
+// Завдання 2
+
+using System;
+
+interface IPaymentMethod
+{
+    void Pay(decimal amount);
+}
+
+class CreditCard : IPaymentMethod
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Оплата кредитною карткою: {amount} грн");
+    }
+}
+
+class PayPal : IPaymentMethod
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Оплата через PayPal: {amount} грн");
+    }
+}
+
+class ApplePay : IPaymentMethod
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Оплата через ApplePay: {amount} грн");
+    }
+}
+
+class Order
+{
+    public IPaymentMethod PaymentMethod { get; set; }
+
+    public void ProcessPayment(decimal amount)
+    {
+        PaymentMethod.Pay(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Order order1 = new Order { PaymentMethod = new CreditCard() };
+        Order order2 = new Order { PaymentMethod = new PayPal() };
+        Order order3 = new Order { PaymentMethod = new ApplePay() };
+
+        order1.ProcessPayment(250.00m);
+        order2.ProcessPayment(150.50m);
+        order3.ProcessPayment(499.99m);
+    }
+}
---
+++ lab19/Program.cs
@@ -0,0 +1,113 @@
+﻿// Завдання 1
+/*
+using System;
+
+class Transport
+{
+    public string Name { get; set; }
+
+    public virtual void Move()
+    {
+        Console.WriteLine($"{Name} рухається...");
+    }
+}
+
+class Car : Transport
+{
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} їде дорогою");
+    }
+}
+
+class Bicycle : Transport
+{
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} їде по велодоріжці");
+    }
+}
+
+class Train : Transport
+{
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} їде по рейках");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Transport[] transports = new Transport[]
+        {
+            new Car { Name = "Автомобіль" },
+            new Bicycle { Name = "Велосипед" },
+            new Train { Name = "Потяг" }
+        };
+
+        foreach (var transport in transports)
+        {
+            transport.Move();
+        }
+    }
+}
+*/
+
+// Завдання 2
+
+using System;
+using System.Collections.Generic;
+
+class Employee
+{
+    public string Name { get; set; }
+
+    public virtual void Work()
+    {
+        Console.WriteLine($"{Name} працює");
+    }
+}
+
+class Programmer : Employee
+{
+    public override void Work()
+    {
+        Console.WriteLine($"{Name} пише код");
+    }
+}
+
+class Designer : Employee
+{
+    public override void Work()
+    {
+        Console.WriteLine($"{Name} створює дизайн");
+    }
+}
+
+class Manager : Employee
+{
+    public override void Work()
+    {
+        Console.WriteLine($"{Name} керує проєктом");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Employee> employees = new List<Employee>
+        {
+            new Programmer { Name = "Микола" },
+            new Designer { Name = "Євгенія" },
+            new Manager { Name = "Богдан" }
+        };
+
+        foreach (var employee in employees)
+        {
+            employee.Work();
+        }
+    }
+}
